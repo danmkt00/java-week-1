19a7ce767a33c462123a8f65e0c2f2c1274ffaa7
danmkt00 <dani4ku@gmail.com>
2025-09-16 11:55:46 +0200

    add 04 exercises

--- 04-control-structures/exercises/IfElseE.java
+++ 04-control-structures/exercises/IfElseE.java
@@ -46,13 +46,60 @@ public class IfElseE
 
         // -------------------- Student Exercises --------------------
         // 1. Check if x is even or odd.
+        int x = 2;
+        if(x%2==0){
+            System.out.println("even");
+        }else{
+            System.out.println("odd");
+        }
 
         // 2. Check age and print "Adult" or "Minor".
+        int age = 18;
+        if(age >= 18){
+            System.out.println("Adult");
+        }else{
+            System.out.println("Minor");
+
+        }
 
         // 3. Assign grades based on score using if-else if-else.
+        int score = 80;
+        char grade;
+        if(score >= 90){
+            grade = 'A';
+        }else if(score>=75){
+            grade = 'B';
+        }else if(score>=50){
+            grade = 'C';
+        }else{
+            grade = 'F';
+        }
+
+        System.out.println("Your grade is "+grade);
 
         // 4. Check temperature and print "Hot", "Warm", or "Cold".
 
+        int temperature = 31;
+        String weather;
+        if(temperature > 30){
+            weather = "Hot";
+        }
+        else if(temperature >= 20){
+            weather = "Warm";
+        }
+        else {
+            weather = "Cold";
+        }
+        System.out.println("The weather is: "+ weather);
+
         // 5. Check if n is positive AND even using logical operators.
+
+        int n = 6;
+        if(n > 0 && n%2==0){
+            System.out.println(n+" is positive and even");
+        }else{
+            System.out.println(n+" is not positive or even");
+        }
+
     }
 }
--- 04-control-structures/exercises/Switch.java
+++ 04-control-structures/exercises/Switch.java
@@ -69,13 +69,127 @@ public class Switch
 
         // -------------------- Student Exercises --------------------
         // 1. Create int month (1-12) and print month name using switch.
+        int month = 2;
+        String monthName;
+        switch (month) {
+            case 1:  monthName = "January";   break;
+            case 2:  monthName = "February";  break;
+            case 3:  monthName = "March";     break;
+            case 4:  monthName = "April";     break;
+            case 5:  monthName = "May";       break;
+            case 6:  monthName = "June";      break;
+            case 7:  monthName = "July";      break;
+            case 8:  monthName = "August";    break;
+            case 9:  monthName = "September"; break;
+            case 10: monthName = "October";   break;
+            case 11: monthName = "November";  break;
+            case 12: monthName = "December";  break;
+            default: monthName = "Invalid month";
+        }
+
+        System.out.println("The month is " + monthName);
+
 
         // 2. Create char grade and print description using switch.
 
+        char grade = 'C';
+        String description;
+
+        switch (grade) {
+            case 'A':
+                description = "Excellent";
+                break;
+            case 'B':
+                description = "Good";
+                break;
+            case 'C':
+                description = "Average";
+                break;
+            case 'D':
+                description = "Below Average";
+                break;
+            case 'F':
+                description = "Fail";
+                break;
+            default:
+                description = "Invalid grade";
+        }
+
+        System.out.println("Grade: " + grade + " -> " + description);
+
+
         // 3. Create int trafficLight (1-3) and print light color using switch.
+
+        int trafficLight = 2;
+        String lightColor;
+
+        switch (trafficLight) {
+            case 1:
+                lightColor = "Red";
+                break;
+            case 2:
+                lightColor = "Yellow";
+                break;
+            case 3:
+                lightColor = "Green";
+                break;
+            default:
+                lightColor = "Invalid light";
+        }
+
+        System.out.println("Traffic Light: " + lightColor);
+
 
         // 4. Create int menuOption (1-5) and print which option is selected.
 
+        int menuOption = 3;
+        String action;
+
+        switch (menuOption) {
+            case 1:
+                action = "Option 1 selected";
+                break;
+            case 2:
+                action = "Option 2 selected";
+                break;
+            case 3:
+                action = "Option 3 selected";
+                break;
+            case 4:
+                action = "Option 4 selected";
+                break;
+            case 5:
+                action = "Option 5 selected";
+                break;
+            default:
+                action = "Invalid option";
+        }
+
+        System.out.println(action);
+
+
         // 5. Combine cases for weekends (6 and 7) vs weekdays (1-5).
+
+        int weekDay = 7;
+        String dayType;
+
+        switch (weekDay) {
+            case 6:
+            case 7:
+                dayType = "Weekend";
+                break;
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                dayType = "Weekday";
+                break;
+            default:
+                dayType = "Invalid day";
+        }
+
+        System.out.println("Day " + weekDay + " -> " + dayType);
+
     }
 }
